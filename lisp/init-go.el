0f091b797a2cab42750b31edeb0bf97e799a37db
Fangjie Zhang <fangjie.zhang@shopee.com>
2019-05-29 14:50:51 +0800

    modify go

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -15,6 +15,7 @@
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize)
   (exec-path-from-shell-copy-env "GOPATH"))
+(add-hook 'before-save-hook 'gofmt-before-save)
 (add-hook 'go-mode-hook (lambda()
                           (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
 
